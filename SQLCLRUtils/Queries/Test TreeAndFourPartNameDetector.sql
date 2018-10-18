4cce0c7af322215b9317420ca4f665db2c64651d
Enrique Catala <enrique@enriquecatala.com>
2018-10-18 12:39:15 +0200

    better test to understand results

--- SQLCLRUtils/Queries/Test TreeAndFourPartNameDetector.sql
+++ SQLCLRUtils/Queries/Test TreeAndFourPartNameDetector.sql
@@ -1,4 +1,12 @@
-﻿select * 
-from (values('select * from bbdd.dbo.table'),('select * from dbo.table'),('select * from server.bbdd.dbo.table')) x(txt)
-cross apply dbo.TreeAndFourPartNameDetector(x.txt) result
-where result.[Group] <> '0'
+﻿SELECT *
+FROM
+(
+    VALUES
+        ('select * from bbdd.dbo.table'),
+        ('select * from rf.UNOx.dbo.MerchTerminalDetail'),
+        ('select * from dbo.table'),
+        ('select * from server.bbdd.dbo.table'),
+		('select * from server2.bbdd2.dbo.table2')
+) x (txt)
+    CROSS APPLY dbo.TreeAndFourPartNameDetector(x.txt) result
+WHERE result.[Group] <> '0';
